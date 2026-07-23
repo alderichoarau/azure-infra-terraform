c384be4a7f26269a3d2879db928541a0644e1efa
Alderic Hoarau <alderic.hoarau@gmail.com>
2026-07-23 16:25:31 +0200

    feat: remove old static web app

--- terraform/static-web-app.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_static_web_app" "stapp" {
-  name                = "stapp-${var.owner}-tf"
-  resource_group_name = data.azurerm_resource_group.rg.name
-  location            = "westeurope"
-  sku_tier            = "Free"
-  sku_size            = "Free"
-  tags                = local.tags
-}
